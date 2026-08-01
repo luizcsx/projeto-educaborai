23e6f2133e5e66466fd6ce5eae0e3e616682903f
Luiz Miguel <contato.luiz404@gmail.com>
2026-08-01 18:15:26 -0300

    #063: Data de lançamento definida para 04/07

--- componentes/js/main.js
+++ componentes/js/main.js
@@ -1,4 +1,4 @@
-        const launchDate = new Date('August 1, 2026 15:40:00').getTime();
+        const launchDate = new Date('August 4, 2026 14:30:00').getTime();
 
         function updateCountdown() {
             const now = new Date().getTime();
